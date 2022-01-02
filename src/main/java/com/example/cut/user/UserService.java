4f3c734ab6b3ba2ef0c5e6a59a1e04b86c1118e2
MASHOD0 <mashhood2002@gmail.com>
2022-01-02 22:52:30 +0530

    UserService.java init

---
+++ src/main/java/com/example/cut/user/UserService.java
@@ -0,0 +1,24 @@
+package com.example.cut.user;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class UserService {
+    public List<User> getUsers(){
+        return List.of(
+                new User(
+                        1,
+                        "Mashhhod",
+                        "3lm",
+                        "pwned",
+                        "a@i.in",
+                        LocalDate.of(2002, 5, 20),
+                        "This is mashhood!",
+                        false
+                ));
+    }
+}
